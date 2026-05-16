f8c52776877bf294b326496c5ab5dc0d764913cb
zyg <akash830ue@gmail.com>
2026-05-16 12:54:44 +0800

    implement optimized Microsoft Clarity tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,15 @@ export default function RootLayout({
     gtag('config', 'G-581CTY5RR0');
   `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wrub6cf27q");
+  `}
+        </Script>
         <Navbar />
         <div className="flex-1">{children}</div>
         <Footer />
